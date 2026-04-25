fb20e1da564b55b477f2959b230874fe19dbd61f
ipz243_kmo <ipz243_kmo@student.ztu.edu.ua>
2026-04-25 18:22:45 +0300

    Template-method

---
+++ kpz_lab3/kpz_lab3/Task5/AddChildCommand.cs
@@ -0,0 +1,16 @@
+﻿class AddChildCommand : ICommand
+{
+    private LightElementNode parent;
+    private LightNode child;
+
+    public AddChildCommand(LightElementNode parent, LightNode child)
+    {
+        this.parent = parent;
+        this.child = child;
+    }
+
+    public void Execute()
+    {
+        parent.AddChild(child);
+    }
+}
